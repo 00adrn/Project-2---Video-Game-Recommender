6f6a8e7153a38e04f0b245cb1bb091625f02191b
axiorty <axxy56769@gmail.com>
2025-11-08 22:14:57 -0500

    Integrate DataManager and kNN recommender setup: added objects need to call function

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from recommender import predict
-
 from fastapi import FastAPI
 import uvicorn
 
+from backend.utils import DataManager
+from backend.knn import kNN
+from backend.recommend import recommend_games
+
+# -----------------------
+# Setup DataManager
+# -----------------------
+dm = DataManager()
+dm.setup(
+    main_data_file='preprocessing/modified_games.json',
+    vector_data_file='preprocessing/games_vectors.parquet'
+)
+
+# -----------------------
+# Setup kNN Recommender
+# -----------------------
+knn = kNN(
+    vectorizer='preprocessing/tfidf_vectorizer.pkl',
+    svd='preprocessing/svd_model.pkl',
+    data_man=dm,
+    lemmatizer='preprocessing/lemmatizer.pkl',
+    stop_words='preprocessing/stop_words.pkl'
+)
+
+
 app = FastAPI()
 
-def func1():
-    return [0, 516, 5, 200, 323, 720]
+recommend_games(knn, dm, input_string, algo_type_flag, sample_size, num_of_games_needed)
+
+
+
+
+# def func1():
+#     return [0, 516, 5, 200, 323, 720]
 
 @app.get("/")
 def root():
@@ -15,9 +43,6 @@ def root():
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
-results = predict(input_string, in_dataset = True)
+# results = predict(input_string, in_dataset = True)
 
 ##show to user
-
-
-
